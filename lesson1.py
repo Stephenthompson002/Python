719325c61d1047a957582d3e7befeec054579714
Stephen Ifeoluwa Thompson <134720609+Stephenthompson002@users.noreply.github.com>
2025-04-28 23:46:04 +0100

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,24 @@
+# This is a comment
+print("Ene Loves Food")  # Prints the string to the console
+
+name = "Ene"  # String variable
+age = 106     # Integer variable
+is_she_a_foodie = True #Boolean variable
+
+
+name = input ("Enter your name:")
+age = input ("Enter your age:")
+print ("I Love " + name + age)
+print ("I am " + age)
+
+x = 5
+y = 12
+
+print (x+y)
+print (x>y)
+print (x<y)
+
+num1 =int(input("Enter your first number:"))
+num2 =int(input("Enter your second number:"))
+
+print ("The Sum is ", num1 + num2)
